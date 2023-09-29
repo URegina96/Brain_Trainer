606387caee897abe5fbe90076612d6931ed146e2
regina <usmanova.re@yandex.ru>
2023-09-29 22:54:38 +0500

    Проработка таймера, рассмотрим не через Handler,  а через объект класса CountDownTimer

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -5,23 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //получаем объект класса = PreferenceManager.getDefaultSharedPreferences(в качестве параметра принимает контекст); - выберем этот способ -самый быстрый, так как нам малого количества данных
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        //для того что бы занести какие-либо данные вызываем первым метод edit() - редактировать , putInt("test"-ключ ,5 - значение) - заносим данные, apply() - метод для сохранения значения
-        preferences.edit().putInt("test", 5).apply();  //даже если закоментировать эту строку  и перезакрыть приложение Toast выдаст число, так как использовали постоянное хранение данных
-        //получаем переменную   getInt - метод для получения необходимого значения указав тип, который нам нужен ("test" - ключ,0-значение по умолчанию, если значение отсутствует)
-        int test = preferences.getInt("test", 0);
-        // в виде Toast выводим значение на экран, Integer.toString(test) - метод для того что бы показать число
-        Toast.makeText(this, Integer.toString(test), Toast.LENGTH_SHORT).show();
+        textViewTimer = findViewById(R.id.textViewTimer);
+        //CountDownTimer  - это абстрактный класс, в котором можно реализовать методы (так как он нам понадобиться только один раз используем анонимный класс)
+        CountDownTimer timer = new CountDownTimer(6000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) { //метод вызывается в каждую секунду
+                //получаем сколько секунд осталось и приводим результат к типу int
+                int seconds = (int) (millisUntilFinished / 1000);
+                seconds++;  //что бы при завершении отсчета не висело 0 на экране долго
+                //выводим на экран
+                textViewTimer.setText(Integer.toString(seconds));
+            }
+
+            @Override
+            public void onFinish() {  //метод , который вызывается при завершении отчета
+                Toast.makeText(MainActivity.this, " таймер завершен ", Toast.LENGTH_SHORT).show();
+                textViewTimer.setText(Integer.toString(0)); //что бы при завершении отсчета не висело 0 на экране долго
+            }
+        };
+        timer.start();
     }
 }
